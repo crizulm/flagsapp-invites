a7036326d307233c5997e551ea77d5b4283ece9b
Matias Crizul <crizulm@gmail.com>
2018-11-24 19:27:45 -0300

    Validations

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,26 +1,32 @@
 class InvitesController < ApplicationController
 
   def create
-    @invite = Invite.new
-    @invite.sender_id = params[:sender_id]
-    @invite.organization_id = params[:organization_id]
-    @invite.email = params[:email]
+    sender_id = params[:sender_id]
+    organization_id = params[:organization_id]
+    email = params[:email]
+    return render json: { error: 'The sender is NULL' }, status: :bad_request if sender_id.nil?
+    return render json: { error: 'The organization is NULL' }, status: :bad_request if organization_id.nil?
+    return render json: { error: 'The email is NULL' }, status: :bad_request if email.nil?
+
+    @invite = Invite.new(sender_id: sender_id, organization_id: organization_id,
+                         email: email)
 
     if @invite.save
       InviteMailer.new_user_invite(@invite).deliver
       render json: @invite, status: :ok
     else
-      render nothing: :true, status: :bad_request
+      return render json: { error: @invite.errors.full_messages }, status: :bad_request
     end
   end
 
   def show
-    @invite = Invite.find_by(token: params[:token])
+    token = params[:token]
+    @invite = Invite.find_by(token: token)
 
     if !@invite.nil?
       render json: @invite, status: :ok
     else
-      render nothing: :true, status: :bad_request
+      return render json: { error: 'The invitation not exist' }, status: :bad_request
     end
   end
 
--- app/models/invite.rb
+++ app/models/invite.rb
@@ -4,6 +4,7 @@ class Invite < ApplicationRecord
   validates :email, presence: true
   validates :organization_id, presence: true
   validates :sender_id, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   def generate_token
     self.token = Digest::SHA1.hexdigest([self.organization_id, Time.now, rand].join)
